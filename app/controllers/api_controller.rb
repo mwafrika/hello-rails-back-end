05113d612c3d9c915a67cc97d97413abd279a573
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-07-13 13:19:12 +0300

    Fix linter errors

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
   include Response
-  
+
   def random_greeting
     greeting = Greeting.find(Greeting.pluck(:id).sample)
     json_response({ message: greeting })
